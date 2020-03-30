373d1b1e0025b77b6a41bd9ab809aaf940d18ff0
Logic Pesto <loic2.petit@epitech.eu>
2020-03-30 16:41:24 +0200

    Patch a function in Gamecore

--- src/GameCore.cpp
+++ src/GameCore.cpp
@@ -166,8 +166,6 @@ GameCore::GameCore(std::string libPath)
         throw(Error(dlerror()));
     dlerror();
     this->Libs["IDisplay"] = getNameFromLibrary(libPath);
-    // this->Libs["IDisplay"] = libPath.substr(libPath.find_last_of('_') + 1,
-    // libPath.find_last_of('.') - libPath.find_last_of('_') - 1);
     this->Libs_names = GetLibName();
     this->Games_names = GetGameName();
 }
