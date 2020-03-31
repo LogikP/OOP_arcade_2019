dc655afe891fa0ece197088d35e752088b497f77
Logic Pesto <loic2.petit@epitech.eu>
2020-03-31 14:26:32 +0200

    Add some function in pacman

--- games/pacman/src/pacman.cpp
+++ games/pacman/src/pacman.cpp
@@ -11,3 +11,29 @@ pacman::pacman()
 {
 
 }
+
+std::string pacman::getName() const
+{
+
+}
+
+std::vector<std::string> pacman::getMap() const
+{
+
+}
+
+int pacman::SendEvent(int key, int toto)
+{
+
+}
+
+void pacman::saveGame(std::string map)
+{
+
+}
+
+//// For the dlsym Symbol /////
+extern "C" pacman *createDisplay()
+{
+    return new pacman;
+}
