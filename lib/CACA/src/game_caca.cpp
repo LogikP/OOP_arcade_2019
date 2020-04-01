5640f77df96bcca1fd5343bff4b3efb8aaf83b71
thomas.durand@epitech.eu <tdurand@thomas.durand>
2020-04-01 23:40:56 +0200

    [ADD] displaying the map of the chosen game

--- lib/CACA/src/game_caca.cpp
+++ lib/CACA/src/game_caca.cpp
@@ -7,14 +7,61 @@
 
 #include "lib_caca.hpp"
 
-int libCaca::InitProg()
+int libCaca::InitProg(std::vector<std::string> map)
 {
+    caca_clear_canvas(window.getCanvas());
+    caca_set_color_ansi(window.getCanvas(), CACA_WHITE, CACA_BLACK);
+    for (int i = 0; i < (int)map.size(); i++)
+        caca_put_str(window.getCanvas(), 0, i, map[i].c_str());
+    caca_refresh_display(this->window.getDisplay());
     return 0;
+}
+
+int libCaca::checkEventGame(int quit)
+{
+    caca_event_t ev;
+    unsigned int const event_mask = CACA_EVENT_KEY_PRESS
+| CACA_EVENT_RESIZE | CACA_EVENT_MOUSE_PRESS | CACA_EVENT_QUIT;
+    int event = caca_get_event(window.getDisplay(),  event_mask, &ev, 0);
+
+
+    while (event) {
+        if (caca_get_event_type(&ev) == CACA_EVENT_KEY_PRESS) {
+            switch (caca_get_event_key_ch(&ev)) {
+                case CACA_KEY_RETURN:
+                    quit = GAME;
+                    break;
+                    break;
+                case 'l':
+                    quit = 4;
+                    break;
+                case CACA_KEY_ESCAPE:
+                    quit = QUIT;
+                    break;
+            }
+            if (caca_get_event_type(&ev) & CACA_EVENT_QUIT)
+                quit = QUIT;
+        }
+        event = caca_get_event(window.getDisplay(), CACA_EVENT_KEY_PRESS, &ev, 0);
+    }
+    return quit;
 }
 
 std::string libCaca::Game()
 {
-    return "toto";
+    int quit = 0;
+
+    caca_set_display_title(window.getDisplay(), "Arcade");
+    caca_set_color_ansi(window.getCanvas(), CACA_BLACK, CACA_WHITE);
+    while (quit != LIB || quit != QUIT || quit != GAME || quit != 4) {
+        quit = this->checkEventGame(quit);
+        if (quit == QUIT)
+            return "kill";
+        caca_clear_canvas(window.getCanvas());
+        caca_set_color_ansi(window.getCanvas(), CACA_WHITE, CACA_BLACK);
+        caca_refresh_display(this->window.getDisplay());
+    }
+    return "success";
 }
 
 void libCaca::initWindow()
